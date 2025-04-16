a53a3372f2417b300b129ee729482319dea1c9f2
Mallamma220 <yerukalvamallamma30@gmail.com>
2025-04-16 11:36:10 +0530

    Login Viwes And Controller Added

---
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/AuthrizationController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+
+namespace FinTrackNewZen.Web.Controllers
+{
+    public class AuthrizationController : Controller
+    {
+        //private readonly ILogger<AuthrizationController> _logger;
+
+        //public AuthrizationController(ILogger<AuthrizationController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+            //return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string emailPhone, string password)
+        {
+            string validEmailPhone = "admin";
+            string validPassword = "admin";
+
+            if (emailPhone == validEmailPhone && password== validPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid login attempt.";
+                return View();
+            }
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string emailPhone)
+        {
+            if (string.IsNullOrEmpty(emailPhone) || string.IsNullOrWhiteSpace(emailPhone))
+            {
+                ViewBag.ErrorMessage = "Please enter your Email or Phone number.";
+                return View();
+            }
+
+
+            ViewBag.EmailPhone = emailPhone;
+            return RedirectToAction("VerifyOTP", "Authrization");
+        }
+
+        public IActionResult VerifyOTP()
+        {
+
+            // ViewBag.EmailPhone = TempData["EmailPhone"]; 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VerifyOTP(string otp)
+        {
+
+            if (!string.IsNullOrEmpty(otp) && otp == "123456")
+            {
+                return RedirectToAction("ResetPassword", "Authrization");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid OTP. Please try again.";
+                return View();
+            }
+        }
+
+        public IActionResult ResetPassword()
+        {
+            // ViewBag.EmailPhone = TempData["EmailPhone"]; 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(string newPassword, string confirmPassword)
+        {
+
+            if (newPassword == confirmPassword)
+            {
+                return RedirectToAction("Login", "Authrization");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Passwords do not match.";
+                return View();
+            }
+        }
+    } 
+}
---
+++ FinTrackNewZen/FinTrackNewZen.Web/wwwroot/js/login.js
@@ -0,0 +1,13 @@
+﻿//document.getElementById("loginForm").addEventListener("submit", function (e) {
+//    e.preventDefault();
+
+//    const emailPhone = document.getElementById("emailPhone").value;
+//    const password = document.getElementById("password").value;
+
+//    if (emailPhone && password) {
+//        alert("Login successful!");
+//        window.location.href = "/Authrization/Test(6)";
+//    } else {
+//        alert("Please fill in all fields.");
+//    }
+//});
