9ecc52f3ea4554cd9db17ba3de64fe26ae7607fa
Ranjith B <ranjith.behera17@gmail.com>
2025-04-17 01:09:18 +0530

    Added View and Edit Fed CSHTML

---
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrackNewZen.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly ILogger<AuthorizationController> _logger;
+
+        public AuthorizationController(ILogger<AuthorizationController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/FederationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrackNewZen.Web.Controllers
+{
+    public class FederationController : Controller
+    {
+        private readonly ILogger<FederationController> _logger;
+
+        public FederationController(ILogger<FederationController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult ViewFederation()
+        {
+            return View();
+        }
+        public IActionResult EditFederation()
+        {
+            return View();
+        }
+    }
+}
