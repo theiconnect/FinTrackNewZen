042fc8c724aa8a4be91da8ea93a1e2e569ca8db9
trivenichennu@123 <trivenichennu408@gmail.com>
2025-04-17 15:55:48 +0530

    Modols ,views,controller and css,image folder created and code also written

---
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinTrackNewZen.Web.Models;
+
+namespace FinTrackNewZen.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login() => View();
+        [HttpPost]
+        public IActionResult Login(UserViewModel model)
+        {
+            // Authenticate
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult ForgotPassword() => View();
+        [HttpPost]
+        public IActionResult ForgotPassword(UserViewModel model)
+        {
+            // Send OTP logic
+            return RedirectToAction("VerifyOtp");
+        }
+
+        public IActionResult VerifyOtp() => View();
+        [HttpPost]
+        public IActionResult VerifyOtp(UserViewModel model)
+        {
+            // Validate OTP
+            return RedirectToAction("ResetPassword");
+        }
+
+        public IActionResult ResetPassword() => View();
+        [HttpPost]
+        public IActionResult ResetPassword(UserViewModel model)
+        {
+            // Reset password logic
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ FinTrackNewZen/FinTrackNewZen.Web/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinTrackNewZen.Web.Models
+{
+    public class UserViewModel
+    {
+        public string EmailOrPhone { get; set; }
+        public string Password { get; set; }
+        public string OTP { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
