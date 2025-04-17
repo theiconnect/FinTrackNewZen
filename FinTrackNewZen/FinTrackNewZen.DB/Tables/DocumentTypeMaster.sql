5011db8bf573a2a6f4760d6c40230c426e83ae95
Ranjith B <ranjith.behera17@gmail.com>
2025-04-17 12:07:51 +0530

    DB Tables added
    DB Tables added

---
+++ FinTrackNewZen/FinTrackNewZen.DB/Tables/DocumentTypeMaster.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE dbo.DocumentTypeMaster
+(
+	DocumentTypeIdPk INT NOT NULL PRIMARY KEY,
+	DocumentTypeCode VARCHAR(100) NOT NULL,
+	DocumentType	VARCHAR(100) NOT NULL
+)
---
+++ FinTrackNewZen/FinTrackNewZen.DB/Tables/FederationDocument.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE TABLE [dbo].[FederationDocument]
+(
+	[FederationDocumentIdPk] INT NOT NULL PRIMARY KEY identity (1,1),
+    DouctmentTypeIdFk int Not Null,
+    DocumentName VARCHAR(256) NOT NULL, 
+    DocumentPath VARCHAR(Max) NOT NULL,  
+    StatusIdFk  int Not Null ,
+    [FederationIdFk] INT NOT NULL,
+    CONSTRAINT fk_FederationDocument_Federation FOREIGN KEY (FederationIdFk) REFERENCES Federation(FederationIdPk),
+    CONSTRAINT fk_FederationDocument_DocumentTypeMaster FOREIGN KEY (DouctmentTypeIdFk) REFERENCES DocumentTypeMaster(DocumentTypeIdPk),
+    CONSTRAINT fk_FederationDocument_StatusMaster FOREIGN KEY (StatusIdFk) REFERENCES StatusMaster([StatusIdPk])
+)
