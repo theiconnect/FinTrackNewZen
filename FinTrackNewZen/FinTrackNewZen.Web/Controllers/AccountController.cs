a349b991a36807f547ebe6987c86b039714c8107
Arunasri19 <gundiboyinaarunasri@gmail.com>
2025-04-17 17:28:44 +0530

    Create a Login Pages  Using in MVC

---
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrackNewZen.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult VerifyOtp()
+        {
+            return View();
+        }
+       
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinTrackNewZen/FinTrackNewZen.Web/wwwroot/js/_LoginLayout.js
@@ -0,0 +1,8 @@
+﻿function showPopup(message) {
+    const toastEl = document.getElementById('popupToast');
+    const toastMsg = document.getElementById('toastMsg');
+    toastMsg.textContent = message;
+
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
