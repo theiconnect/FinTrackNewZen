7b55d1c25c2ef35356afc228518f3695028bb403
Arunasri19 <gundiboyinaarunasri@gmail.com>
2025-04-22 16:01:48 +0530

    modified the files

--- FinTrackNewZen/FinTrackNewZen.Web/Controllers/AccountController.cs
+++ FinTrackNewZen/FinTrackNewZen.Web/Controllers/AccountController.cs
@@ -8,20 +8,75 @@ namespace FinTrackNewZen.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(string emailPhone, string password)
+        {
+            if (!string.IsNullOrEmpty(emailPhone) && !string.IsNullOrEmpty(password))
+            {
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid login attempt.";
+                return View();
+            }
+        }
 
         public IActionResult ForgotPassword()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult ForgotPassword(string emailPhone)
+        {
+            if (string.IsNullOrEmpty(emailPhone) || string.IsNullOrWhiteSpace(emailPhone))
+            {
+                ViewBag.ErrorMessage = "Please enter your Email or Phone number.";
+                return View();
+            }
+
+            TempData["EmailPhone"] = emailPhone;
+            return RedirectToAction("VerifyOTP", "Account");
+        }
+
 
         public IActionResult VerifyOtp()
         {
             return View();
         }
-       
+        [HttpPost]
+        public IActionResult VerifyOTP(string otp)
+        {
+            if (!string.IsNullOrEmpty(otp) && otp == "123456")
+            {
+                return RedirectToAction("ResetPassword", "Account");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid OTP. Please try again.";
+                return View();
+            }
+        }
+
         public IActionResult ResetPassword()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult ResetPassword(string newPassword, string confirmPassword)
+        {
+            if (newPassword == confirmPassword)
+            {
+
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Passwords do not match.";
+                return View();
+            }
+        }
+
     }
 }
