88c3e5ae969474d3e2b9360bef07886fa2de0e93
arjuniconnect <arjunk.24a.iconnect@outlook.com>
2025-04-23 12:55:13 +0530

    Added seed data and created rolemaster table

---
+++ FinTrackNewZen/FinTrackNewZen.DB/Scripts/PostDeploy/LoadUser.sql
@@ -0,0 +1,28 @@
+﻿INSERT INTO [dbo].[User]
+(
+    UserRole,
+    EmailAddress,
+    Phone,
+    [PasswordHash],
+    OTP,
+    CreatedByIdFk,
+    CreatedOn,
+    LastUpdatedByIdFk,
+    LastUpdatedOn,
+    EmployeeIdFk,
+    RoleIdFk
+)
+VALUES
+(
+    'Admin',                     
+    'admin@fintrack.com',       
+    '1234567890',               
+    'Welcome@01',               
+    '123456',                   
+    1,                          
+    GETDATE(),                  
+    NULL,                       
+    NULL,                       
+    1  ,
+    1
+);
